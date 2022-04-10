b04c571bbe981feb662473b6e0148f30ad783bf7
vnrdev <froggerbin@gmail.com>
2022-04-10 16:12:07 +0200

    added
    Spring Boot from scratch

---
+++ Spring/Boot/Demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+/**
+ * @author vnrdev
+*/
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
