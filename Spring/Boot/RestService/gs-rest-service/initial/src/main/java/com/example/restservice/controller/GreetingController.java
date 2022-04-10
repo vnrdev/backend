faa8b3547879a15cb5d6fab8ad6dc7934eaf3c3f
vnrdev <froggerbin@gmail.com>
2022-04-10 23:03:43 +0200

    added

---
+++ Spring/Boot/RestService/gs-rest-service/initial/src/main/java/com/example/restservice/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.restservice.controller;
+
+/**
+ * @author vnrdev
+*/
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.restservice.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
